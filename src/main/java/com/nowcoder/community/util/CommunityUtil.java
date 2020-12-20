fc028bf4b320735b0d932efbac89274fb425c2b1
2738292360@qq.com <2738292360@qq.com>
2020-12-20 13:04:42 +0800

    I have known the git

--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -44,12 +43,4 @@ public class CommunityUtil {
     public static String getJSONString(int code) {
         return getJSONString(code, null, null);
     }
-
-    public static void main(String[] args) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "zhangsan");
-        map.put("age", 25);
-        System.out.println(getJSONString(0, "ok", map));
-    }
-
 }
